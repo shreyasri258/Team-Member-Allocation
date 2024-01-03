a60480e2d2cd6ea52f8ef7c19e08eb8e02dc08dd
shreyasri258 <kertishreya2508@gmail.com>
2024-01-03 12:45:37 +0530

    Setup and First Steps

---
+++ teammemberlllocation/src/App.js
@@ -0,0 +1,22 @@
+import './App.css'
+import * as React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Header from './Header';
+import Footer from './Footer';
+import Content from './Content';
+import Employees  from './Employees';
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+   
+    
+     <h1><Employees></Employees></h1>
+      <Footer></Footer>
+    </div>
+   
+    
+  );
+}
---
+++ teammemberlllocation/src/Content.js
@@ -0,0 +1,6 @@
+const Content = () => {
+    return(
+  <content><h1>Content</h1></content>
+  
+    );}
+  export default Content;
