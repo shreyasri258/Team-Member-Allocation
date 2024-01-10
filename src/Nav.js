62aeaa06aa415da30ecb320eea607189c6bc7365
shreyasri258 <kertishreya2508@gmail.com>
2024-01-10 11:56:24 +0530

    Final Changes

--- src/Nav.js
+++ src/Nav.js
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg gradient-custom border border-dark text-primary">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
+        <li className="nav-item text-danger">
           <Link className="nav-link" to="/">
-            Home
+        <h3 >Home</h3>
           </Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/GroupedTeamMembers">
-            Teams
+           <h3>Teams</h3> 
           </Link>
         </li>
       </ul>
